dec24ecd21692959deba1f261d80e20bdd1a387b
Matt92Martin <matt92martin@gmail.com>
2018-02-10 21:41:49 -0800

    Arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello TypeScript!');
+const pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
+
+const mappedPizzas = pizzas.map(({ name }) => name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: "Blazing Inferno",
+  getName: () => pizza.name
+};
+
+console.log(pizza.getName());
